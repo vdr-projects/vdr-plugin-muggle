9056b7936ce029d76c8d9e7dea912a6a88b41b01
lvw <lvw@e10066b5-e1e2-0310-b819-94efdf66514b>
2004-09-30 21:41:15 +0000

    Added function to escape query strings
    git-svn-id: https://vdr-muggle.svn.sourceforge.net/svnroot/vdr-muggle/trunk/muggle-plugin@192 e10066b5-e1e2-0310-b819-94efdf66514b

--- mg_database.c
+++ mg_database.c
@@ -1,13 +1,11 @@
-/*******************************************************************/
 /*! \file   mg_database.c
  *  \brief  A capsule around MySql database access
- ******************************************************************** 
+ *
  * \version $Revision: 1.2 $
  * \date    $Date$
  * \author  Ralf Klueber, Lars von Wedel, Andreas Kellner
  * \author  file owner: $Author$
  */
-/*******************************************************************/
 
 #include "mg_database.h"
 
@@ -27,22 +25,20 @@ mgDB::~mgDB()
 {
 }
 
-string mgDB::escape_string( MYSQL *db, string s )
-{
-  char *escbuf = (char *) malloc( 2*s.length() + 1 );
-
-  int len = mysql_real_escape_string( db, escbuf, s.c_str(), s.length() );
-
-  string res( escbuf );
-  free( resbuf );
-
-  return res;
-}
-
 MYSQL mgDB::getDBHandle()
 {
 
   return m_dbase;
 }
 
+string mgDB::escape_string( MYSQL *db, string s )
+{
+  char *escbuf = (char *) malloc( 2*s.size() + 1 );
 
+  int len = mysql_real_escape_string( db, escbuf, s.c_str(), s.size() );
+
+  string r = string( escbuf );
+  free( escbuf );
+
+  return r;
+}
