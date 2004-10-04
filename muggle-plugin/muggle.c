0a097cb09ab2ec5751f4ce42192d3b0a9d80ca8d
lvw <lvw@e10066b5-e1e2-0310-b819-94efdf66514b>
2004-10-04 22:16:03 +0000

    Going to 0.0.5
    git-svn-id: https://vdr-muggle.svn.sourceforge.net/svnroot/vdr-muggle/trunk@201 e10066b5-e1e2-0310-b819-94efdf66514b

--- muggle-plugin/muggle.c
+++ muggle-plugin/muggle.c
@@ -22,7 +22,7 @@
 #include <getopt.h>
 #include <config.h>
 
-static const char *VERSION        = "0.0.4";
+static const char *VERSION        = "0.0.5";
 static const char *DESCRIPTION    = "Media juggle plugin for VDR";
 static const char *MAINMENUENTRY  = "Muggle";
 
