852203bca4fc6b391e6d5533aaec2bfce2a036df
lvw <lvw@e10066b5-e1e2-0310-b819-94efdf66514b>
2004-10-09 08:07:30 +0000

    Remove errror in muggle.c
    git-svn-id: https://vdr-muggle.svn.sourceforge.net/svnroot/vdr-muggle/trunk@214 e10066b5-e1e2-0310-b819-94efdf66514b

--- muggle-plugin/muggle.c
+++ muggle-plugin/muggle.c
@@ -22,15 +22,7 @@
 #include <getopt.h>
 #include <config.h>
 
-<<<<<<< .mine
-<<<<<<< .mine
-static const char *VERSION        = "0.0.3";
-=======
-static const char *VERSION        = "0.0.4";
-=======
-static const char *VERSION        = "0.0.5";
->>>>>>> .r202
->>>>>>> .r177
+static const char *VERSION        = "0.0.6";
 static const char *DESCRIPTION    = "Media juggle plugin for VDR";
 static const char *MAINMENUENTRY  = "Muggle";
 
