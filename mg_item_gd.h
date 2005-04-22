44eaaa9a438815af55428d568b70c16b658dec3c
wr61 <wr61@e10066b5-e1e2-0310-b819-94efdf66514b>
2005-04-22 03:22:09 +0000

    forgot to add mg_item_gd.ch
    git-svn-id: https://vdr-muggle.svn.sourceforge.net/svnroot/vdr-muggle/branches/0.1.7-wr@650 e10066b5-e1e2-0310-b819-94efdf66514b

---
+++ mg_item_gd.h
@@ -0,0 +1,79 @@
+/*!
+ * \file mg_item.h
+ * \brief A general interface to data items, currently only GiantDisc
+ *
+ * \version $Revision: 1.0 $
+ * \date    $Date: 2004-12-07 10:10:35 +0200 (Tue, 07 Dec 2004) $
+ * \author  Wolfgang Rohdewald
+ * \author  Responsible author: $Author: wr $
+ *
+ */
+
+#ifndef _MG_ITEM_GD_H
+#define _MG_ITEM_GD_H
+#include <stdlib.h>
+#include <string>
+#include <mysql/mysql.h>
+
+using namespace std;
+
+#include "mg_tools.h"
+#include "mg_valmap.h"
+#include "mg_item.h"
+
+//! \brief represents a GiantDisc content item 
+class mgItemGd : public mgItem
+{
+    public:
+	mgListItem* getKeyItem(mgKeyTypes kt);
+
+	//! \brief copy constructor
+        mgItemGd(const mgItemGd* c);
+
+	//! \brief construct an item from an SQL row
+        mgItemGd (const MYSQL_ROW row);
+
+	mgItemGd* Clone();
+
+//! \brief returns filename
+        string getSourceFile (bool AbsolutePath=true) const;
+
+//! \brief returns artist
+        string getArtist () const
+        {
+            return m_artist;
+        }
+
+//! \brief returns the name of the album
+        string getAlbum () const;
+
+//! \brief returns the name of genre
+        string getGenre () const;
+
+//! \brief returns the bitrate
+        string getBitrate () const;
+
+//! \brief returns the file name of the album image
+        string getImageFile () const;
+
+//! \brief returns samplerate
+        int getSampleRate () const;
+
+//! \brief returns # of channels
+        int getChannels () const;
+
+    protected:
+	void InitFrom(const mgItemGd* c);
+    private:
+        mutable string m_mp3file;
+        string m_artist;
+        string m_albumtitle;
+        string m_genre2_id;
+        string m_genre2;
+        string m_bitrate;
+        int m_samplerate;
+        int m_channels;
+};
+
+extern mgListItem zeroitem;
+#endif
