3f97eebd6d0c8c74c54ebbd01b49dd0649e7c4fe
scop <scop@e10066b5-e1e2-0310-b819-94efdf66514b>
2008-01-01 11:41:49 +0000

    Rename image_convert.sh to muggle-image-convert.
    git-svn-id: https://vdr-muggle.svn.sourceforge.net/svnroot/vdr-muggle/trunk/muggle-plugin@970 e10066b5-e1e2-0310-b819-94efdf66514b

--- mg_image_provider.c
+++ mg_image_provider.c
@@ -362,7 +362,7 @@ void mgImageProvider::Action()
 
 
 	  char *tmp;
-	  asprintf( &tmp, "image_convert.sh \"%s\" \"%s\"", filename.c_str(), tmpFile.c_str() );
+	  asprintf( &tmp, "muggle-image-convert \"%s\" \"%s\"", filename.c_str(), tmpFile.c_str() );
 	  system( (const char*) tmp );
 	  free(tmp);
 	 
